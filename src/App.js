ab666c94ab1e440595b376e190a5f408543854f8
NwaguChima <nwaguchima48@gmail.com>
2022-11-09 01:34:07 +0100

    added app style setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import styles from "./App.module.scss";
+
 function App() {
-  return <div className="App">Hello React</div>;
+  return (
+    <div className={styles.App}>
+      Hello React Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Laborum sed quibusdam nulla veritatis facilis cum, perspiciatis tenetur
+      voluptatum et possimus soluta ratione at adipisci amet, ipsum quo. Sit,
+      tempora voluptate. Hello React Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Laborum sed quibusdam nulla veritatis facilis cum,
+      perspiciatis tenetur voluptatum et possimus soluta ratione at adipisci
+      amet, ipsum quo. Sit, tempora voluptate.
+    </div>
+  );
 }
 
 export default App;
