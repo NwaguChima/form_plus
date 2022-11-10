d70e074c12b49c649ef70d13f8d68c8d2a6c7a81
NwaguChima <nwaguchima48@gmail.com>
2022-11-10 18:34:43 +0100

    styled top template

--- src/components/templates/Templates.jsx
+++ src/components/templates/Templates.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./Templates.module.scss";
 
 const Templates = () => {
-  return <div>Templates</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.container__heading}>
+        <h2>All Templates</h2>
+        <p>2000 templates</p>
+      </div>
+      <div className={styles.container__list}></div>
+    </div>
+  );
 };
 
 export default Templates;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import InfoItem from "../components/infoItem/InfoItem";
 import SearchNav from "../components/searchNav/SearchNav";
+import Templates from "../components/templates/Templates";
 
 const Home = () => {
   return (
     <div>
       <SearchNav />
       <InfoItem />
+      <Templates />
     </div>
   );
 };
