1694b625ff8f4509f736ef6a9016af82d4335b6a
NwaguChima <nwaguchima48@gmail.com>
2022-11-10 18:14:04 +0100

    completed filters

--- src/components/infoItem/InfoItem.jsx
+++ src/components/infoItem/InfoItem.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BsInfoCircle } from "react-icons/bs";
+import styles from "./InfoItem.module.scss";
 
 const InfoItem = () => {
-  return <div>InfoItem</div>;
+  return (
+    <div className={styles.info}>
+      <i>
+        <BsInfoCircle />
+      </i>
+      <p>
+        Tada! Get started with a free template. Can't find what you are looking?
+        Search from the 1000+ available templates
+      </p>
+    </div>
+  );
 };
 
 export default InfoItem;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import InfoItem from "../components/infoItem/InfoItem";
 import SearchNav from "../components/searchNav/SearchNav";
 
 const Home = () => {
   return (
     <div>
       <SearchNav />
+      <InfoItem />
     </div>
   );
 };
