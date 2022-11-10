8bad086b7efdfc6e067ed36bd9c34b0250739c79
NwaguChima <nwaguchima48@gmail.com>
2022-11-10 20:23:33 +0100

    styled buttons

--- src/components/paginate/Paginate.jsx
+++ src/components/paginate/Paginate.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import styles from "./Paginate.module.scss";
+import { MdOutlineNavigateNext, MdOutlineNavigateBefore } from "react-icons/md";
 
 const Paginate = () => {
   return (
     <div className={styles.paginate}>
-      <button>previous</button>
+      <div className={styles.paginate__prev}>
+        <i>
+          <MdOutlineNavigateBefore />
+        </i>
+        <button>Previous</button>
+      </div>
       <div className={styles.paginate__item}>
         <span>1</span>
         <span>of</span>
         <span>14</span>
       </div>
-      <button>next</button>
+      <div className={styles.paginate__next}>
+        <button>Next</button>
+        <i>
+          <MdOutlineNavigateNext />
+        </i>
+      </div>
     </div>
   );
 };
