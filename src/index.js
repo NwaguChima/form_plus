dfc5f34b9a5bc7ff634bb5ba1eaf8de0c6144795
NwaguChima <nwaguchima48@gmail.com>
2022-11-12 07:42:18 +0100

    added store

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
