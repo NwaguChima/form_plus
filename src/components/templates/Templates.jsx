f5e1a87cf2f5ae6ee6ef59828513d29cec197f0c
NwaguChima <nwaguchima48@gmail.com>
2022-11-13 01:07:44 +0100

    sorted

--- src/components/templates/Templates.jsx
+++ src/components/templates/Templates.jsx
@@ -55,16 +55,57 @@ const Templates = () => {
     setSearchedTemplates(templateArr);
   }, [search, filteredTemplates]);
 
-  useEffect(() => {}, [order, date, filteredTemplates]);
+  function handleOrder(inputArr, order, date) {
+    let outputArr = [...inputArr];
+
+    if (order !== "Default") {
+      switch (order) {
+        case "Ascending":
+          return outputArr.sort((a, b) => a.name.localeCompare(b.name));
+        case "Descending":
+          return outputArr.sort((a, b) => b.name.localeCompare(a.name));
+        default:
+          return outputArr;
+      }
+    } else if (date !== "Default") {
+      switch (date) {
+        case "Ascending":
+          return outputArr.sort((a, b) => {
+            let aDate = Date.now(a.createdAt);
+            let bDate = Date.now(b.createdAt);
+
+            return aDate - bDate;
+          });
+        case "Descending":
+          return outputArr.sort((a, b) => {
+            let aDate = Date.now(a.createdAt);
+            let bDate = Date.now(b.createdAt);
+
+            return bDate - aDate;
+          });
+
+        default:
+          return outputArr;
+      }
+    }
+
+    return outputArr;
+  }
+
+  useEffect(() => {
+    const sortedTemplates = handleOrder(searchedTemplates, order, date);
+
+    setOrderedTemplates(sortedTemplates);
+  }, [order, date, searchedTemplates]);
 
   return (
     <div className={styles.container}>
       <div className={styles.container__heading}>
         <h3>{category} Templates</h3>
-        <p>{searchedTemplates.length} templates</p>
+        <p>{orderedTemplates.length} templates</p>
       </div>
       <div className={styles.container__list}>
-        {searchedTemplates.slice(0, 100).map((temp) => {
+        {orderedTemplates.slice(0, 100).map((temp) => {
           const { name, link, description, id } = temp;
           return (
             <Template
