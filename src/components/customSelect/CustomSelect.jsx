b2f4be8c3895eb289f53839a1542306f2bfe99b9
NwaguChima <nwaguchima48@gmail.com>
2022-11-14 13:11:54 +0100

    cleaned up old select implementaion

--- src/components/customSelect/CustomSelect.jsx
+++ src/components/customSelect/CustomSelect.jsx
@@ -1,46 +1,12 @@
 import React from "react";
-import Select from "react-select";
 import styles from "./CustomSelect.module.scss";
 
 const CustomSelect = ({ options, label, handleChange, value, name }) => {
-  const defaultValue = (options, value) => {
-    return options ? options.find((option) => option.value === value) : "";
-  };
-
-  const selectStyles = {
-    control: (base, state) => {
-      return {
-        ...base,
-        width: "100%",
-        height: "40%",
-        border: 0,
-        boxShadow: "none",
-        fontSize: "12px",
-        paddingLeft: "10px",
-      };
-    },
-
-    option: (provided, { isFocused, isSelected }) => ({
-      ...provided,
-      color: isSelected ? "white" : "#3f3e3e",
-      background: isSelected ? "#3f3e3e" : isFocused ? "#e5e5e5" : "white",
-    }),
-  };
-
   return (
     <div className={styles.select}>
       <fieldset>
         <legend>{label}</legend>
-        <div className={styles.selectholder}>
-          {/* <Select
-            value={defaultValue(options, value) || null}
-            options={options}
-            styles={selectStyles}
-            onChange={(value) => handleChange(value)}
-            components={{
-              IndicatorSeparator: () => null,
-            }}
-          /> */}
+        <div className={styles.select__holder}>
           <select name={name} onChange={(e) => handleChange(e)}>
             {options.map((option) => (
               <option key={option.value} value={option.value}>
